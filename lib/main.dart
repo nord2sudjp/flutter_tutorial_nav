a339056e957e8993f422f0e9ca6cb3fe97aa5faa
maka y <nord2sudjp@gmail.com>
2020-11-08 18:29:18 +0900

    AppBar

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.menu),
+          title: const Text('AppBar'),
+          backgroundColor: Colors.orange,
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.face, color: Colors.white,),
+            ),
+            IconButton(
+              icon: Icon(Icons.email, color: Colors.white,),
+            ),
+            IconButton(
+              icon: Icon(Icons.favorite, color: Colors.white,),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
