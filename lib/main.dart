415b33e55984b9832073b2ae1e35563dd40a1a0e
maka y <nord2sudjp@gmail.com>
2020-11-08 19:40:07 +0900

    BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,119 @@ void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.menu),
-          title: const Text('AppBar'),
-          backgroundColor: Colors.orange,
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.face, color: Colors.white,),
+      title: _title,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // 表示中の Widget を取り出すための index としての int 型の mutable な stored property
+  int _selectedIndex = 0;
+
+  // 表示する Widget の一覧
+  static List<Widget> _pageList = [
+    CustomPage(pannelColor: Colors.cyan, title: 'Home'),
+    CustomPage(pannelColor: Colors.green, title: 'Settings'),
+    CustomPage(pannelColor: Colors.pink, title: 'Search')
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _onIconPressed(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: const Text('AppBar'),
+        backgroundColor: Colors.orange,
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.face, color: Colors.white,),
+            onPressed: () {_onIconPressed(0);}
+
+          ),
+          IconButton(
+            icon: Icon(Icons.email, color: Colors.white,),
+              onPressed: () {_onIconPressed(1);}
+          ),
+          IconButton(
+            icon: Icon(Icons.favorite, color: Colors.white,),
+              onPressed: () {_onIconPressed(2);}
+          ),
+        ],
+      ),
+      body: _pageList[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Setting'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text('Search'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class CustomPage extends StatelessWidget {
+  final Color pannelColor;
+  final String title;
+
+  CustomPage({@required this.pannelColor, @required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleTextStyle = Theme.of(context).textTheme.title;
+    return Container(
+      child: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+              color: pannelColor,
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: titleTextStyle.fontSize,
+                color: titleTextStyle.color,
+              ),
             ),
-            IconButton(
-              icon: Icon(Icons.email, color: Colors.white,),
-            ),
-            IconButton(
-              icon: Icon(Icons.favorite, color: Colors.white,),
-            ),
-          ],
+          ),
         ),
       ),
     );
